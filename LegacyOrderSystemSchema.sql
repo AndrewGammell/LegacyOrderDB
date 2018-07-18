948c678ddd5f6f85b56ce9ee2f5d7ec8d1ecc359
AndrewGammell <andrew.gammell@sogeti.com>
2018-07-18 11:05:51 +0100

    Updated schema to include customerId columns on the orders and ordersDetails tables. Added the foreign key to the orderId columns of orders to ordersDetails. Updated the insert statements to include the customerId.

--- LegacyOrderSystemSchema.sql
+++ LegacyOrderSystemSchema.sql
@@ -1,5 +1,6 @@
+CREATE DATABASE `legacy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 
-CREATE TABLE `orders` (
+CREATE TABLE legacy.`orders` (
   `orderId` int(11) NOT NULL DEFAULT '-1',
   `dateOrdered` date NOT NULL,
   `dateReceived` date DEFAULT NULL,
@@ -8,12 +9,13 @@ CREATE TABLE `orders` (
   `createdDate` date NOT NULL,
   `updatedStaff_id` int(11) DEFAULT NULL,
   `updatedDate` date DEFAULT NULL,
+  `customerId` int(11) NOT NULL DEFAULT '-1',
   PRIMARY KEY (`orderId`),
   UNIQUE KEY `orderId_UNIQUE` (`orderId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `ordersDetails` (
-  `orderId` int(11) NOT NULL,
+CREATE TABLE legacy.`ordersDetails` (
+  `orderId` int(11) NOT NULL DEFAULT '-1',
   `productId` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `unitPrice` int(11) NOT NULL,
@@ -21,12 +23,13 @@ CREATE TABLE `ordersDetails` (
   `createdDate` date NOT NULL,
   `updatedStaffId` int(11) DEFAULT NULL,
   `updatedDate` date DEFAULT NULL,
+  `customerId` int(11) NOT NULL,
   PRIMARY KEY (`orderId`),
   UNIQUE KEY `orderId_UNIQUE` (`orderId`),
-  CONSTRAINT `orderId` FOREIGN KEY (`orderId`) REFERENCES `orders` (`orderId`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `orderId` FOREIGN KEY (`orderId`) REFERENCES `orders` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `person` (
+CREATE TABLE legacy.`person` (
   `id` int(11) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) NOT NULL,
@@ -39,8 +42,8 @@ CREATE TABLE `person` (
   UNIQUE KEY `ID_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `legacy`.`orders` (`orderId`, `dateOrdered`, `status`, `createdStaffId`, `createdDate`) VALUES ('123', '2018-06-21', 'SHIPPED', '456', '2018-06-21');
+INSERT INTO `legacy`.`orders` (`orderId`, `dateOrdered`, `status`, `createdStaffId`, `createdDate`, `customerId`) VALUES ('123', '2018-06-21', 'SHIPPED', '456', '2018-06-21', '123');
 
-INSERT INTO `legacy`.`ordersDetails` (`orderId`, `productId`, `quantity`, `unitPrice`, `createdStaffId`, `createdDate`) VALUES ('123', '001', '100', '1', '456', '2018-06-21');
+INSERT INTO `legacy`.`ordersdetails` (`orderId`, `productId`, `quantity`, `unitPrice`, `createdStaffId`, `createdDate`, `customerId`) VALUES ('123', '1', '100', '1', '456', '2018-06-21', '123');
 
-INSERT INTO `legacy`.`person` (`id`, `firstname`, `lastname`, `email`, `password`, `dob`, `lastCreated`) VALUES ('987', 'John', 'Doe', 'test@test.test', 'secret', '1952-07-10', '2018-06-21');
+INSERT INTO `legacy`.`person` (`id`, `firstname`, `lastname`, `email`, `password`, `dob`, `lastCreated`) VALUES ('123', 'John', 'Doe', 'test@test.test', 'secret', '1952-07-10', '2018-06-21');
