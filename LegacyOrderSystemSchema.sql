49a5a2f2b3cbb3e777d00d853b7bcd7fc5de1602
AndrewGammell <andrew.gammell@sogeti.com>
2018-08-03 11:02:40 +0100

    fixed typo and inserting order

--- LegacyOrderSystemSchema.sql
+++ LegacyOrderSystemSchema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `legacy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE `legacy` /*!40100 DEFAULT CHARACTER SET utf8mb4  */;
 
 use legacy;
 
@@ -12,12 +12,11 @@ CREATE TABLE `orders` (
   `updatedStaffId` int(11) DEFAULT NULL,
   `updatedDate` date DEFAULT NULL,
   `customerId` int(11) NOT NULL DEFAULT '-1',
-  `next_val` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`orderId`),
   UNIQUE KEY `orderId_UNIQUE` (`orderId`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `orders_details` (
+CREATE TABLE `ordersDetails` (
   `orderId` int(11) NOT NULL DEFAULT '-1',
   `productId` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
@@ -29,7 +28,7 @@ CREATE TABLE `orders_details` (
   `customerId` int(11) NOT NULL,
   PRIMARY KEY (`orderId`),
   UNIQUE KEY `orderId_UNIQUE` (`orderId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
 CREATE TABLE `person` (
@@ -43,11 +42,11 @@ CREATE TABLE `person` (
   `lastUpdated` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ID_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
-INSERT INTO `legacy`.`orders` (`orderId`, `dateOrdered`, `status`, `createdStaffId`, `createdDate`, `customerId`) VALUES ('2018-06-21', 'SHIPPED', '456', '2018-06-21', '123');
+INSERT INTO `legacy`.`orders`(`orderId`,`dateOrdered`,`dateReceived`,`status`,`createdStaffId`,`createdDate`,`updatedStaffId`,`updatedDate`,`customerId`)VALUES('123','2018-06-21' ,'2018-06-21','SHIPPED','456','2018-06-21',null,null,'123');
 
-INSERT INTO `legacy`.`ordersdetails` (`orderId`, `productId`, `quantity`, `unitPrice`, `createdStaffId`, `createdDate`, `customerId`) VALUES ('1', '1', '100', '1', '456', '2018-06-21', '123');
+INSERT INTO `legacy`.`ordersdetails` (`orderId`, `productId`, `quantity`, `unitPrice`, `createdStaffId`, `createdDate`, `customerId`) VALUES ('123', '1', '100', '1', '456', '2018-06-21', '123');
 
-INSERT INTO `legacy`.`person` (`id`, `firstname`, `lastname`, `email`, `password`, `dob`, `lastCreated`) VALUES ('1', 'John', 'Doe', 'test@test.test', 'secret', '1952-07-10', '2018-06-21');
+INSERT INTO `legacy`.`person` (`id`, `firstname`, `lastname`, `email`, `password`, `dob`, `lastCreated`) VALUES ('123', 'John', 'Doe', 'test@test.test', 'secret', '1952-07-10', '2018-06-21');
